9b6adc81c620538995609ce63ba2c354e4723c45
Ricardo Fantinelli <45544590+ricarfantinelli@users.noreply.github.com>
2021-03-24 20:59:35 +0100

    change config
    Changing a piece in config.

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'article-cms-sqlserver.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'article-cms-db'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'ricaradmin'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ricar*1234'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'xxxxxxxxxxx'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
